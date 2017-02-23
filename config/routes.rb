ee2430cde80addf25faeb342d966207da9bda7c3
Scott <scottmail88@gmail.com>
2017-02-23 02:41:43 +0000

    Checkpoint 18 CRUD complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
+  resources :posts
   
-  get 'welcome/contact'
+  get 'about' => 'welcome#about'
   
-  get 'welcome/faq'
+ # get 'welcome/contact'
+  
+  # 'welcome/faq'
 
   root 'welcome#index'
   
