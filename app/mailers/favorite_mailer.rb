3915b69b5644615d2d4ce38488b9d553f1ed3994
Scott <scottmail88@gmail.com>
2017-03-31 14:30:04 +0000

    Assignment 31 Favorites Complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+  
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@bloccit.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.com>"
+    headers["References"] = "<post/#{post.id}@bloccit.com>"
+    
+    @post = post
+    
+    mail(to: post.user.email, subject: "You're following, #{post.title}!")
+  end
 end
