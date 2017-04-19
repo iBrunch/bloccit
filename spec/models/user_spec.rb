a94aaeaacfd02f6dc6014549d65643e02c6868e8
Scott <scottmail88@gmail.com>
2017-04-19 06:00:31 +0000

    Add blank email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,8 +30,9 @@ RSpec.describe User, type: :model do
   end
   
   describe "invalid user" do
-    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com", password: "helloworld") }
+    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "", password: "helloworld") }
+    let(:user_with_invalid_password) { User.new(name: "Bloccit User", email: "user@bloccit.com", password: "") }
 
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
@@ -39,6 +40,10 @@ RSpec.describe User, type: :model do
   
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
+    end
+    
+    it "should be an invalid user due to blank password" do
+      expect(user_with_invalid_password).to_not be_valid
     end
 
   end
