e853bc3ad409567982739e59e1ddf2ec31d41e8d
Scott <scottmail88@gmail.com>
2017-05-18 03:51:07 +0000

    Remove unnecessary elsif

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,8 +2,6 @@ module PostsHelper
   def is_user_authorized_for_posts?(post, action)
     if current_user && (current_user == post.user || is_admin_or_moderator(action))
       true
-     elsif current_user
-      false
     end
   end
 end
